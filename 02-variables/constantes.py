df4b875105058e6c55086e875a9701fd2e6ca038
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-06-18 10:47:24 +0200

    Constantes

---
+++ 02-variables/constantes.py
@@ -0,0 +1,12 @@
+# Constantes en python
+import math
+
+PI = math.pi
+print("PI:", PI)
+
+NOMBRE_BASE_DATOS = "clientes_db"
+print("Base de Datos:", NOMBRE_BASE_DATOS)
+
+# Esto NO SE DEBE HACER
+NOMBRE_BASE_DATOS = "cambio_clientes_db"
+print("Base de Datos:", NOMBRE_BASE_DATOS)
