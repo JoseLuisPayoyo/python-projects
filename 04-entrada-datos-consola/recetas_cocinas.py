c8030866986a2e905492dc0664e103d1a555b57c
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-06-23 18:03:32 +0200

    Ejemplo recetas terminado

---
+++ 04-entrada-datos-consola/recetas_cocinas.py
@@ -0,0 +1,11 @@
+nombre_receta = input("Ingresa el nombre: ")
+ingredientes = input("Ingresa los ingredientes: ")
+tiempo_preparacion = int(input("Ingresa el tiempo de preparacion (min): "))
+dificultad = input("Ingresa la dificultad: ")
+
+# imprimir variables
+print("------------")
+print(f"Nombre de la receta: {nombre_receta}")
+print(f"Ingredientes: {ingredientes}")
+print(f"Tiempo de preparacion: {tiempo_preparacion}")
+print(f"Dificultad: {dificultad}")
