ceed8df4d5cbdf96965ecb156e6396503bb1ed46
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-06-23 18:34:29 +0200

    asignacion encadenada

---
+++ 05-operadores/asignacion.py
@@ -0,0 +1,8 @@
+texto = "Hola"
+
+a, b, c = 10, 20, 30
+print(f"A: {a}, B: {b}, C:{c}")
+
+# asignacion encaenada
+a = b = c = 10
+print(f"A: {a}, B: {b}, C:{c}")
---
+++ 05-operadores/tipos.py
@@ -0,0 +1,19 @@
+# suma -> +
+# resta -> -
+# multiplicacion -> *
+# division -> /
+# division entera -> //
+# modulo -> %
+# potencia -> **
+
+a = 10
+b = 3
+
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b:.2f}")
+print(f"{a} // {b} = {a // b}")
+print(f"{a} % {b} = {a % b}")
+print(f"{a} ** {b} = {a ** b}")
+
