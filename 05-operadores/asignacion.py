3206c78e2e281379fc055dcd1bd9903674c7ef58
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-06-23 18:40:11 +0200

    asignacion

--- 05-operadores/asignacion.py
+++ 05-operadores/asignacion.py
@@ -6,3 +6,15 @@ print(f"A: {a}, B: {b}, C:{c}")
 # asignacion encaenada
 a = b = c = 10
 print(f"A: {a}, B: {b}, C:{c}")
+
+# intercambio de valores sin variable auxiliar
+x, y = 5, 10
+print(f"x = {x}, y = {y}")
+
+# cambiamos los valores
+x, y = y, x
+print(f"x = {x}, y = {y}")
+
+# recibir multiples valores por separado
+nombre, apellido = input("Nombre y apellido separados por comas: ").split(",")
+print(f"Nombre: {nombre.strip()}\nApellido: {apellido.strip()}")
