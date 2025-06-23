58d86aa36b94fe112cd06f9ab18ff6d7c53ce2ab
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-06-23 17:38:11 +0200

    Ejemplo terminado

---
+++ 04-entrada-datos-consola/conversion_tipos_datos.py
@@ -0,0 +1,10 @@
+# convertir string a numero
+numero_string = "10"
+numero_int = int(numero_string)
+
+# string a decimal
+numero_decimal = float(numero_string)
+
+# int a decimal
+numero_str = str(numero_int) 
+
---
+++ 04-entrada-datos-consola/ejemplo.py
@@ -0,0 +1,15 @@
+# ejemplo de concatenar o sumar valores
+num1_cadena = "10"
+print(f"Numero 1 en cadena: {num1_cadena}")
+
+num2_cadena = "20"
+print(f"Numero 1 en cadena: {num2_cadena}")
+
+resultado = num1_cadena + num2_cadena
+print(f"Concatenar: {resultado}")
+
+# conversion
+num1_int = int(num1_cadena)
+num2_int = int(num2_cadena)
+resultado_int = num1_int + num2_int
+print(f"Sumar: {resultado_int}")
