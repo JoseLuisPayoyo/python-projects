e5a86ace8ec7cb85c70197bf8122903e44f71224
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-06-23 18:46:33 +0200

    asignacion compuesta

---
+++ 05-operadores/asignacion_compuestos.py
@@ -0,0 +1,21 @@
+a, b = 10, 3
+print(f"Valor inicial a: {a}, b: {b}")
+
+# operador compuesto suma (+=)
+a += b # a = a + b
+print(f"a += b = {a}")
+a = 10 # reiniciamos valor de a
+
+# operador compuesto resta (-=)
+a -= b # a = a- b
+print(f"a -= b = {a}")
+a = 10 # reiniciamos valor de a
+
+# operador compuesto multiplicacion (*=)
+a *= b # a = a * b
+print(f"a *= b = {a}")
+a = 10 # reiniciamos valor de a
+
+# operador compuesto division (/=)
+a /= b # a = a / b
+print(f"a /= b = {a:.2f}")
