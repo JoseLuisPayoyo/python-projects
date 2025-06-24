a8f50553d415323d590e3f07114d6acc5a92fbf9
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-06-24 11:18:56 +0200

    if y else visto

---
+++ 06-sentencias-control/sentencia_elif.py
@@ -0,0 +1,3 @@
+print("*** Sentencia elif ***")
+
+edad = int(input("Tu edad: "))
---
+++ 06-sentencias-control/sentencia_if.py
@@ -0,0 +1,8 @@
+print("*** Sentencia if ***")
+
+edad = int(input("Tu edad: "))
+
+if edad >= 18:
+    print(f"Eres mayor de edad. Tienes {edad} años")
+else:
+    print(f"Eres menor de edad. Tienes {edad} años")  
