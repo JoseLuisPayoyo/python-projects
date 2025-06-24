6f2ea05d7da67a74de5d47dd3adeb3ea80fdac76
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-06-24 11:22:15 +0200

    elif visto

--- 06-sentencias-control/sentencia_elif.py
+++ 06-sentencias-control/sentencia_elif.py
@@ -1,3 +1,10 @@
 print("*** Sentencia elif ***")
 
 edad = int(input("Tu edad: "))
+
+if edad < 13:
+    print(f"Eres un niño. Tienes {edad} años")  
+elif 13 <= edad < 18:
+    print(f"Eres un adolescentes. Tienes {edad} años") 
+else:
+    print(f"Eres mayor de edad. Tienes {edad} años")  
